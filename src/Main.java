ee0f36df03f1d0cb75f2a519a9538ec0030849cf
Aikeldi <aykeldi@inbox.ru>
2022-08-12 12:53:49 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ko");
+       return;
+    }
+    public static String homeworkSecond () {
+        int age = 22;
+        int temperature = 43;
+        if (age > 20 && age < 45 && temperature > -20 && temperature < 30 ){
+            return ("Можно идти гулять");
+        } else if (age < 20 && temperature > 0 && temperature < 28);{
+            return ("Можно идти гулять");
+        } else if (age > 45 && temperature > -10 && temperature < 25);{
+            return ("Можно идти гулять");
+        } else {
+            System.out.println("Оставайтесь дома");
+        }
+
+    main(66,44,"apple");
+        main(34,54,"dragon");
+        main(23,54,"gum");
+        main(34,23,"lim");
+        main(22,11,"feet");
+
+    }
+
+    public static void main(int num1, int num2, String word){
+        System.out.println(word + (num1 + num2));
+    }
+
+}
